38f806070eb9b9b2e1a215643bf077c6e4a3d2e5
Tom McGee <tmmcgee13@aol.com>
2015-07-09 10:13:20 -0500

    Lots of changed added a computer, GET request to sinatra!

--- src/Board.js
+++ src/Board.js
@@ -6,9 +6,11 @@ function makeBoard(boardSize)
     htm += '<tr id= "row' + i + '">';
      for (j = 0; j < boardSize; j++)
      {
-      htm += '<td class="square square' + (j + (i * boardSize)) + '"align = "center"style="background-color:#66CD00;font-size:400%;width:100px;border-radius:25px;height:100px;border:3px solid #000;margin: 10px;"></td>'
+      htm += '<td class="square square' + (j + (i * boardSize)) + '" id="' + (j + (i * boardSize)) + '" align = "center"style="background-color:#66CD00;font-size:400%;width:100px;border-radius:25px;height:100px;border:3px solid #000;margin: 10px;"></td>'
      } 
   }
   htm += "</table>"
   return htm;
 }
+
+
